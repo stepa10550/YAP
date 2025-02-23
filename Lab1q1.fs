c4439a437cf74d903552e9167ee1f4dae6095637
stepa10550 <stepa10550.55@gmail.com>
2025-02-23 20:48:16 +0500

    Лабораторная работа 1

---
+++ Lab1q1.fs
@@ -0,0 +1,40 @@
+﻿open System
+
+let isf (x: char) = 
+    match x with
+    |'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|'0'|'-'|'+'|'.'-> true
+    | _ -> false
+
+let isN (x: char) = 
+    match x with
+    |'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|'0' -> true
+    | _ -> false
+
+let isFloat (x: char list) =
+    List.fold (fun a b -> a && b) true (List.map isf x)
+
+let isInt (x: char list) = 
+    List.fold (fun a b -> a && b) true (List.map isN x)
+
+let F x n = [    
+    for i in [1..n] do
+        yield x
+]
+
+let y (x: string) = [
+    for i in [0..x.Length-1] do
+        yield x[i]
+]
+
+
+printf "Введите повторяющееся число: "
+let x = (Console.ReadLine())
+
+printf "Введите колличество чисел: "
+let n = (Console.ReadLine())
+
+if isFloat(y(x)) && isInt(y(n)) then
+    printfn "Ваш список"
+    printf "%A" (F (float x) ((int) n))
+else 
+    printf "Вы ввели неверные данные"
---
+++ Lab1q2.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+let isN (x: char) = 
+    match x with
+    |'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|'0' -> true
+    | _ -> false
+
+let isInt (x: char list) = 
+    List.fold (fun a b -> a && b) true (List.map isN x)
+
+let y (x: string) = [
+    for i in [0..x.Length-1] do
+        yield x[i]
+]
+
+let rec Prod x = 
+    if x > 0 then
+        Prod (x/10) * (x%10)
+    else
+        1
+
+
+printf "Введите натуральное число: "
+let n = (Console.ReadLine())
+
+if isInt(y(n)) then
+    printf "Произведение цифр числа = %d" (Prod ((int)n))
+else 
+    printf "Вы ввели неверные данные"
