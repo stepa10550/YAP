a24be7306d0180a64ee831872389f14d628aad70
stepa10550 <stepa10550.55@gmail.com>
2025-05-12 01:45:51 +0500

    Лабораторная работа 9

---
+++ Lab9/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lab9;
+
+namespace Lab9
+{
+    public partial class MainWindow : Window
+    {
+        private Money money;
+         
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateMoney_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                money = new Money(CheckEnteryData.CheckEnteryUint(RublesInput.Text), 
+                    CheckEnteryData.CheckEnteryByte(KopeksInput.Text));
+                OutputText.Text = $"Создана куча: {money}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Increment_Click(object sender, RoutedEventArgs e)
+        {
+            if (money == null)
+            {
+                MessageBox.Show($"Ошибка: куча не создана, чтобы с ней работать",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            money++;
+            OutputText.Text = $"С ещё одной копейкой: {money}";
+        }
+
+        private void Decrement_Click(object sender, RoutedEventArgs e)
+        {
+            if (money == null)
+            {
+                MessageBox.Show($"Ошибка: куча не создана, чтобы с ней работать",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            money--;
+            OutputText.Text = $"Без одной копейки: {money}";
+        }
+
+        private void AddKopeks_Click(object sender, RoutedEventArgs e)
+        {
+            if (money == null)
+            {
+                MessageBox.Show($"Ошибка: куча не создана, чтобы с ней работать",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            money = money.AddKopeks(CheckEnteryData.CheckEnteryUint(AddKopeksInput.Text));
+            OutputText.Text = $"После добавки копеек: {money}";
+        }
+
+        private void AddItself_Click(object sender, RoutedEventArgs e)
+        {
+            if (money == null)
+            {
+                MessageBox.Show($"Ошибка: куча не создана, чтобы с ней работать",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                var tempMoney = new Money(CheckEnteryData.CheckEnteryUint(RublesInput2.Text),
+                        CheckEnteryData.CheckEnteryByte(KopeksInput2.Text));
+                money = money + tempMoney;
+                OutputText.Text = $"Сложение куч: {money}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SubtractSelf_Click(object sender, RoutedEventArgs e)
+        {
+            if (money == null)
+            {
+                MessageBox.Show($"Ошибка: куча не создана, чтобы с ней работать",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                var tempMoney = new Money(CheckEnteryData.CheckEnteryUint(RublesInput2.Text),
+                        CheckEnteryData.CheckEnteryByte(KopeksInput2.Text));
+                money = money - tempMoney;
+                OutputText.Text = $"Вычитание куч: {money}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ Lab9/Money.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab9
+{
+    internal class Money
+    {
+        private uint rubles;
+        private byte kopeks;
+
+        public Money() 
+        { 
+            rubles = 0;
+            kopeks = 0;
+        }
+
+        public Money(uint rubles, byte kopeks)
+        {
+            if (kopeks >= 100)
+            {
+                this.rubles = rubles + 1;
+                this.kopeks = (byte)(kopeks % 100);
+            }
+            else
+            {
+                this.rubles = rubles;
+                this.kopeks = kopeks;
+            }
+        }
+
+        public Money(Money money)
+        {
+            this.rubles = money.rubles;
+            this.kopeks = money.kopeks;
+        }
+
+        public byte Kopeks 
+        { 
+            get 
+            { 
+                return kopeks; 
+            } 
+            set 
+            {
+                if (kopeks < 100 && kopeks >= 0) 
+                { 
+                    this.kopeks = value; 
+                    return; 
+                }
+                if (kopeks >= 100) 
+                    Console.WriteLine("Копеек больше 100");
+                if (kopeks < 0) 
+                    Console.WriteLine("Копеек не может быть меньше 0");
+                Environment.Exit(-1);
+            } 
+        }
+        public uint Rubles 
+        { 
+            get 
+            { 
+                return rubles; 
+            } 
+            set 
+            {
+                if (kopeks < 0)
+                {
+                    Console.WriteLine("Копеек больше 100");
+                    Environment.Exit(-1);
+                }
+                this.rubles = value; 
+            } 
+        }
+
+        public static Money operator +(Money m1, Money m2)
+        {
+            int resultKopeks = m1.Kopeks + m2.Kopeks;
+            uint resultRubles = m1.Rubles + m2.Rubles + (uint)(resultKopeks / 100);
+            return new Money(resultRubles, (byte)(resultKopeks % 100));
+        }
+
+        public static Money operator -(Money a, Money b)
+        {
+            int Result = 100 * (int)(a.Rubles - b.Rubles) + (a.Kopeks - b.Kopeks);
+            if (Result < 0) 
+                return new Money(0, 0);
+            return new Money((uint)(Result / 100), (byte)(Result % 100));
+        }
+
+        public static explicit operator uint(Money a)
+        {
+            return a.Rubles;
+        }
+
+        public static implicit operator double(Money b)
+        {
+            return b.Kopeks / 100.0;
+        }
+
+        public static Money operator ++(Money a)
+        {
+            a.kopeks++;
+            a.Rubles += (uint)a.Kopeks / 100;
+            a.kopeks %= 100;
+            return a;
+        }
+
+        public static Money operator --(Money a)
+        {
+            if (a.kopeks > 0) a.kopeks--;
+            else if (a.Rubles > 0)
+            {
+                a.rubles--;
+                a.kopeks = 99;
+            }
+            return a;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Rubles} рублей {this.Kopeks} копеек";
+        }
+
+        public Money AddKopeks(uint Kopeks)
+        {
+            uint Result = 100*this.rubles + this.Kopeks + Kopeks;
+            return new Money(Result / 100, (byte)(Result % 100));
+        }
+    }
+}
